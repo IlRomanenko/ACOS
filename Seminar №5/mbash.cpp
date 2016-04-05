6d969fc034264443a4d85a5b64d422dbe71b5430
IlRomanenko <ilya.i.romanenko@yandex.ru>
2016-04-05 21:11:45 +0400

    Updated bash

--- Seminar №5/mbash.cpp
+++ Seminar №5/mbash.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include <string>
+#include <string.h>
 #include <sstream>
 #include <queue>
 //#include <io.h>
@@ -30,21 +31,30 @@ int main()
 {
     string s;
 
-    while (true)
+   // while (true)
     {
         getline(cin, s);
        // cout << s << endl;
-        if (s == "exit")
-            break;
-
+     //   if (s == "exit")
+     //       break;
         int res = fork();
         if (res == 0)
         {
         	//cout << "ok" << endl;
             auto argv = split(s);
+            if (argv.size() == 0)
+            	exit(0);
             string name = argv.front();
+            {
+            	FILE *prog = fopen(name.c_str(), "r");
+            	if (prog == NULL)
+            	{
+            		fclose(prog);
+            		exit(0);
+            	}
+            }
             argv.pop();
-            string params = name;
+            vector<string> params{ name };
             while (!argv.empty())
             {
                 s = argv.front();
@@ -67,11 +77,19 @@ int main()
                     dup2(descr_out, dst_out);
                 }
                 else
-                    params += " " + s;
+                	params.push_back(s);
             }
-           // cout << name << ' ' << params << endl << endl;
-            execl(name.c_str(), params.c_str());
+            char **prms = new char*[params.size()];
+            for (int i = 0; i < (int)params.size(); i++)
+            {
+            	prms[i] = new char[params[i].size() + 1];
+            	strcpy(prms[i], params[i].c_str());
+            	cout << params[i] <<  "//";
+            }
+            cout << endl;
+            execve(name.c_str(), prms, NULL);
         }
+        cout << endl << endl;
     }
 
     return 0;
