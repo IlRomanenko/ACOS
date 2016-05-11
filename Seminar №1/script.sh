cf70db1d6c0f3d34334c954c8041d599f7d51974
ilya <ilya@debian>
2016-05-11 22:19:45 +0300

    Updated GET query

--- Seminar №1/script.sh
+++ Seminar №1/script.sh
@@ -1,5 +1,6 @@
 function genQuery {
-    echo -e "GET " $1 " HTTP/1.0\n\n"  >query
+    echo -e "GET " $2 " HTTP/1.1\nHost: "\
+     $1 "\nAccept: text/html\nConnection: close\n\n"  >query
 }
 
 echo "/" >internallinks
@@ -13,7 +14,7 @@ while read line; do
     ndir=$home$line
     echo $ndir
     mkdir -p $ndir
-    genQuery $line
+    genQuery $1 $line
     nc $1 80 <query >webpage
     cp webpage $ndir$webpage
     
@@ -21,7 +22,7 @@ while read line; do
     awk 'match($0, /\"\/[^"]*\"/){print substr($0, RSTART+1, RLENGTH-2)}' links > n_internallinks
 
     while read new_line; do
-        if ! grep -q $new_line internallinks ; then
+        if ! grep -q "$new_line" internallinks ; then
             echo $new_line >> internallinks
         fi
     done < n_internallinks
--- Seminar №7/netcat.cpp
+++ Seminar №7/netcat.cpp
@@ -15,87 +15,89 @@ using namespace std;
 
 void check_error(int rc)
 {
-	if (rc == -1)
-	{
-		cout << strerror(errno) << endl;
-		exit(0);
-	}
+    if (rc == -1)
+    {
+        cout << strerror(errno) << endl;
+        exit(0);
+    }
 }
 
 const int BUF_SIZE = 1024;
 
 char text[BUF_SIZE];
 
+
+
 string readAll()
 {
-	string res = "", t;
-	while (!cin.eof())
-	{
-		getline(cin, t);
-		res += t + "\n";
-	}
-	return res;
+    string res = "", t;
+    while (!cin.eof())
+    {
+        getline(cin, t);
+        res += t + "\n";
+    }
+    return res;
 }
 
 int main(int argc, char** argv)
 {
-	if (argc != 3)
-	{
-		cout << "Wrong amount of arguments" << endl;
-		cout << "Use as host port" << endl;
-		exit(0);
-	}
+    if (argc != 3)
+    {
+        cout << "Wrong amount of arguments" << endl;
+        cout << "Use as <hostname> <port>" << endl;
+        exit(0);
+    }
 
-	string query = readAll();
+    string query = readAll();
 
-	addrinfo hints, *result, *rp;
-	memset(&hints, 0, sizeof(addrinfo));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = 0;
-	hints.ai_canonname = NULL;
-	hints.ai_addr = NULL;
-	hints.ai_next = NULL;
+    addrinfo hints, *result, *rp;
+    memset(&hints, 0, sizeof(addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = 0;
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
 
-	int socket_fd = -1, rc = 0;
+    int socket_fd = -1, rc = 0;
 
-	rc = getaddrinfo(argv[1], argv[2], NULL, &result);
-	if (rc != 0)
-	{
-		cout << "rc = " << rc << endl;
-		cout << "getaddrinfo: " << gai_strerror(rc) << endl;
-		exit(-1);
-	}
+    rc = getaddrinfo(argv[1], argv[2], NULL, &result);
+    if (rc != 0)
+    {
+        cout << "rc = " << rc << endl;
+        cout << "getaddrinfo: " << gai_strerror(rc) << endl;
+        exit(-1);
+    }
 
-	for (rp = result; rp != NULL; rp = rp->ai_next)
-	{
-		socket_fd = socket(rp->ai_family, rp->ai_socktype,
-		 			rp->ai_protocol);
-		if (socket_fd == -1)
-			continue;
-		if (connect(socket_fd, rp->ai_addr, rp->ai_addrlen) != -1)
-			break;
-		close(socket_fd);
-	}
+    for (rp = result; rp != NULL; rp = rp->ai_next)
+    {
+        socket_fd = socket(rp->ai_family, rp->ai_socktype,
+                    rp->ai_protocol);
+        if (socket_fd == -1)
+            continue;
+        if (connect(socket_fd, rp->ai_addr, rp->ai_addrlen) != -1)
+            break;
+        close(socket_fd);
+    }
 
-	freeaddrinfo(result);
-	if (socket_fd == -1)
-	{
-		cout << "Can't connect to " << argv[1] << endl;
-		exit(0);
-	}
+    freeaddrinfo(result);
+    if (socket_fd == -1)
+    {
+        cout << "Can't connect to " << argv[1] << endl;
+        exit(0);
+    }
 
 
-	string addres(argv[1]);
+    string addres(argv[1]);
 
-	write(socket_fd, query.c_str(), query.length());
+    write(socket_fd, query.c_str(), query.length());
 
-	int nread;
-	while ((nread = recv(socket_fd, text, BUF_SIZE, MSG_WAITALL)) > 0)
-	{
-		for (int i = 0; i < nread; i++)
-			putchar(text[i]);
-	}
+    int nread;
+    while ((nread = recv(socket_fd, text, BUF_SIZE, MSG_WAITALL)) > 0)
+    {
+        for (int i = 0; i < nread; i++)
+            putchar(text[i]);
+    }
 
-	return 0;
+    return 0;
 }
